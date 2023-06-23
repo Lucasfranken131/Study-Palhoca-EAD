d27182b3a70aa4f4640f8ca22f0da5cd67cd6a7e
cadu220 <114344241+cadu220@users.noreply.github.com>
2023-06-22 23:00:37 -0300

    Update Salvar_cidade.java

--- Salvar/Salvar_cidade.java
+++ Salvar/Salvar_cidade.java
@@ -27,24 +27,63 @@ public class Salvar_cidade {
     }
 
 
-    public static void salvar(String[] novo){
+    // public static void salvar(String[] novo){
 
+    //     try {
+    //         //aqui ele vai pegar o conteúdo que já foi salvo, rodando a
+    //         // função "pegar_conteúdo()" e juntando com o conteudo atual passado
+    //         String nova_cidade=novo[0]+","+novo[1];
+    //         String conteudo=pegar_conteudo()+nova_cidade;
+
+    //         //aqui ele está abrindo o arquivo, porém, ele precisa estar nesta sequência,
+    //         //uma vez que se o mesmo arquivo for aberto duas vezes, na segunda vez ele estará vazio.
+    //         //como o "pegar_conteúdo()" também abre o arquivo, primeiro é necessário chamar esta função
+    //         //e só depois abrir aqui.
+    //         FileWriter arquivoEscrita = new FileWriter(caminhoArquivo);
+    //         BufferedWriter escritor = new BufferedWriter(arquivoEscrita);
+
+    //         //escreve o conteúdo que foi pego
+    //         escritor.write(conteudo);
+    //         //fecha o arquivo para poder rodar outra função
+    //         escritor.close();
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+    public static void salvar(String[] novo) {
         try {
-            //aqui ele vai pegar o conteúdo que já foi salvo, rodando a
-            // função "pegar_conteúdo()" e juntando com o conteudo atual passado
-            String nova_cidade=novo[0]+","+novo[1];
-            String conteudo=pegar_conteudo()+nova_cidade;
+            String nova_cidade = novo[0] + "," + novo[1] + "," + novo[2];
+            String conteudo = pegar_conteudo();
 
-            //aqui ele está abrindo o arquivo, porém, ele precisa estar nesta sequência,
-            //uma vez que se o mesmo arquivo for aberto duas vezes, na segunda vez ele estará vazio.
-            //como o "pegar_conteúdo()" também abre o arquivo, primeiro é necessário chamar esta função
-            //e só depois abrir aqui.
+            if (conteudo.contains(novo[0] + ",")) {
+                // O registro já existe, substitui as informações existentes pelas novas
+                String[] linhas = conteudo.split("\n");
+                StringBuilder novoConteudo = new StringBuilder();
+                boolean substituiu = false;
+
+                for (String linha : linhas) {
+                    if (linha.startsWith(novo[0] + ",")) {
+                        novoConteudo.append(nova_cidade).append("\n");
+                        substituiu = true;
+                    } else {
+                        novoConteudo.append(linha).append("\n");
+                    }
+                }
+
+                if (!substituiu) {
+                    // Se o registro não foi substituído, adiciona as informações ao conteúdo existente
+                    novoConteudo.append(nova_cidade).append("\n");
+                }
+
+                conteudo = novoConteudo.toString();
+            } else {
+                // O registro não existe, adiciona as informações ao conteúdo existente
+                conteudo += nova_cidade + "\n";
+            }
+
             FileWriter arquivoEscrita = new FileWriter(caminhoArquivo);
             BufferedWriter escritor = new BufferedWriter(arquivoEscrita);
-
-            //escreve o conteúdo que foi pego
             escritor.write(conteudo);
-            //fecha o arquivo para poder rodar outra função
             escritor.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -69,6 +108,29 @@ public class Salvar_cidade {
         } catch (IOException e) {
             e.printStackTrace();
             return "99999";
+        }
+    }
+
+      public static String buscar_cidade_por_codigo(String codigo) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo));
+            String linha;
+
+            while ((linha = reader.readLine()) != null) {
+                String[] campos = linha.split(",");
+                String codigoCidade = campos[0].trim();
+
+                if (codigoCidade.equals(codigo)) {
+                    reader.close();
+                    return linha;
+                }
+            }
+
+            reader.close();
+            return null; // Estudante não encontrado
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null; // Erro na leitura do arquivo
         }
     }
 
