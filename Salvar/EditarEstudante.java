3a765bb3d65c269c63ff3f384bb5e21c5fa66b14
cadu220 <114344241+cadu220@users.noreply.github.com>
2023-07-01 18:31:19 -0300

    Update EditarEstudante.java

--- Salvar/EditarEstudante.java
+++ Salvar/EditarEstudante.java
@@ -7,6 +7,8 @@ public class EditarEstudante implements ActionListener {
 
         String codigo = JOptionPane.showInputDialog("Insira o código do estudante a ser editado:");
         String idCodigo = codigo;
+        String[] opcoes = {"Sim", "Não"};
+    	String novoAlunoBolsista;
 
         // Buscar o estudante pelo código no arquivo de texto
         String estudanteEncontrado = Salvar_aluno.buscar_estudante_por_codigo(idCodigo);
@@ -19,19 +21,38 @@ public class EditarEstudante implements ActionListener {
             String emailAtual = campos[3].trim();
             String senhaAtual = campos[4].trim();
             String cidadeAtual = campos[5].trim();
+            String bolsistaAtual = campos[6].trim();
 
             // Exibir caixas de diálogo com os valores existentes para permitir a edição
             String novoNome = JOptionPane.showInputDialog("Nome atual: " + nomeAtual + "\nInsira o novo nome do estudante:");
             String novaDataNascimento = JOptionPane.showInputDialog("Data de Nascimento atual: " + dataNascimentoAtual + "\nInsira a nova data de nascimento do estudante:");
             String novoEmail = JOptionPane.showInputDialog("Email atual: " + emailAtual + "\nInsira o novo email do estudante:");
             String novaSenha = JOptionPane.showInputDialog("Senha atual: " + senhaAtual + "\nInsira a nova senha do estudante:");
-            String novaCidade = JOptionPane.showInputDialog("Cidade atual: " + cidadeAtual + "\nInsira a nova cidade do estudante:");
+          //  String novoBolsista = JOptionPane.showInputDialog("Bolsista atual: " + bolsistaAtual + "\nInsira a nova cidade do estudante:");
 
-            String[] array = {codigo, novoNome, novaDataNascimento, novoEmail, novaSenha, novaCidade};
+            String cidade_nome=""; 
+            String[] cidades= Salvar_cidade.pegar_conteudo().split("\n");
+            for (int i =0;i<cidades.length;i++) {
+                cidade_nome += cidades[i].split(",")[1]+",";
+            }
+            
+            String[] novaOption = cidade_nome.split(",");
+
+            int x = JOptionPane.showOptionDialog(null, "Cidade atual: " + cidadeAtual + "\nInsira a nova cidade do estudante:",
+                    "Selecionar cidade",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, novaOption, novaOption[0]);
+            
+            int escolha = JOptionPane.showOptionDialog(null, "O aluno é Bolsista?", "Aluno Bolsista", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+            if (escolha == 0) {
+            	novoAlunoBolsista = "Sim";
+            }else novoAlunoBolsista = "Não";
+
+            String[] array = {codigo, novoNome, novaDataNascimento, novoEmail, novaSenha, novaOption[x], novoAlunoBolsista};
 
             Salvar_aluno.salvar(array);
 
-       
+            // Após salvar as informações atualizadas no arquivo, você pode exibir uma mensagem informando
+            // que a edição foi realizada com sucesso ou qualquer outra ação necessária.
             JOptionPane.showMessageDialog(null, "Estudante editado com sucesso!");
         } else {
             // Estudante não encontrado
