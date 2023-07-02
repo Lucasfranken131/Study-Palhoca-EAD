2cc8d04eec49a3dda708ce725719f4364052ce3e
cadu220 <114344241+cadu220@users.noreply.github.com>
2023-07-02 09:58:06 -0300

    obrigarnomeCriarCidade.java

--- Parte_Grafica/CriarCidade.java
+++ Parte_Grafica/CriarCidade.java
@@ -5,7 +5,14 @@ import javax.swing.JOptionPane;
 public class CriarCidade implements ActionListener {
     public void actionPerformed(ActionEvent ActionEvent) {
         //Aqui pega os valores para a classe Cidade
-        String nome = JOptionPane.showInputDialog("Insira o nome da cidade");
+       String nome = "";
+	//Obriga o usuário a colocar um nome para a cidade
+    	while (nome.isEmpty()) {
+    	    nome = JOptionPane.showInputDialog("Insira o nome da cidade");
+    	    if (nome.isEmpty()) {
+    	        JOptionPane.showMessageDialog(null, "O nome da cidade não pode estar vazio. Por favor, digite um valor válido.");
+    	    }
+    	}
         String uf = JOptionPane.showInputDialog("Insira o estado:");
         //Criação do objeto.
         String[] array = {Salvar_cidade.pegar_cod(),nome,uf};
@@ -13,7 +20,18 @@ public class CriarCidade implements ActionListener {
 
         Salvar_cidade.salvar(array);
 
-	JOptionPane.showMessageDialog(null, "Estudante Criado com sucesso!");
+	// JOptionPane.showMessageDialog(null, "Cidade Criada com sucesso!"
+ //        		+ "\n Você será direcionado para criação de 4 estudantes para a Cidade");
+
+        //Obriga o usuário a criar 4 estudantes para a cidade
+  //       for (int i = 0; i<4; i++) {
+  //       	// Criar uma instância da classe CriarEstudante
+  //           CriarEstudante criarEstudante = new CriarEstudante();
+  //           System.out.println(nome);
+  //           criarEstudante.setOptions(new String[]{nome});
+  //           // Chamar o método actionPerformed da instância de CriarEstudante
+  //           criarEstudante.actionPerformed(ActionEvent);
+  //		}
 	}
 }
 
